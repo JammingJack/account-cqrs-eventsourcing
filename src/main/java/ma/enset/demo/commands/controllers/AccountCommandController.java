31ab21584394af9872526d8571462ae9286018ea
JammingJack <amineseth1@gmail.com>
2021-12-24 12:13:11 +0100

    added an exception handler that relays the exception's message to the client side

--- src/main/java/ma/enset/demo/commands/controllers/AccountCommandController.java
+++ src/main/java/ma/enset/demo/commands/controllers/AccountCommandController.java
@@ -5,10 +5,9 @@ import ma.enset.demo.commonapi.commands.CreateAccountCommand;
 import ma.enset.demo.commonapi.dtos.CreateAccountRequestDTO;
 import org.axonframework.commandhandling.CommandBus;
 import org.axonframework.commandhandling.gateway.CommandGateway;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
@@ -28,5 +27,14 @@ public class AccountCommandController {
                 request.getCurrency()
         ));
         return commandResponde;
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> exceptionHandler(Exception e){
+        ResponseEntity<String> responseEntity = new ResponseEntity<>(
+                e.getMessage(),
+                HttpStatus.INTERNAL_SERVER_ERROR
+        );
+        return responseEntity;
     }
 }
