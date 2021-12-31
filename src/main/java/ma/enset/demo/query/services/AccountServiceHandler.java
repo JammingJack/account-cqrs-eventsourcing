8b0007b98c7f3872f209be3800b252e4c5e61d15
JammingJack <amineseth1@gmail.com>
2021-12-31 02:21:35 +0100

    updating quering db after AccountActivatedEvent

--- src/main/java/ma/enset/demo/query/services/AccountServiceHandler.java
+++ src/main/java/ma/enset/demo/query/services/AccountServiceHandler.java
@@ -2,6 +2,7 @@ package ma.enset.demo.query.services;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import ma.enset.demo.commonapi.events.AccountActivatedEvent;
 import ma.enset.demo.commonapi.events.AccountCreatedEvent;
 import ma.enset.demo.commonapi.events.AccountCreditedEvent;
 import ma.enset.demo.query.entities.Account;
@@ -9,13 +10,16 @@ import ma.enset.demo.query.repositories.AccountRepository;
 import ma.enset.demo.query.repositories.OperationRepository;
 import org.axonframework.eventhandling.EventHandler;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @AllArgsConstructor
 @Slf4j
+@Transactional
 public class AccountServiceHandler {
     private AccountRepository accountRepository;
     private OperationRepository operationRepository;
+
     @EventHandler
     public void on(AccountCreatedEvent event){
         log.info("*******************************");
@@ -26,6 +30,15 @@ public class AccountServiceHandler {
         account.setCurrency(event.getCurrency());
         account.setStatus(event.getStatus());
         account.setOperations(null);
+        accountRepository.save(account);
+    }
+
+    @EventHandler
+    public void on(AccountActivatedEvent event){
+        log.info("*******************************");
+        log.info("AccountActivatedEvent received");
+        Account account = accountRepository.findById(event.getId()).get();
+        account.setStatus(event.getStatus());
         accountRepository.save(account);
     }
 }
